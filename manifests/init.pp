5fbd60f0308ac6e99e540c030a97633e4791a36b
Jonas Platte <mail@jonasplatte.de>
2015-12-04 20:01:15 +0100

    Start adding some classes (I'll definitely try Salt now)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,73 @@
+###################
+# Nginx Webserver #
+###################
+class webserver() {
+  package { 'nginx':
+    require => Exec['apt-update'],
+    ensure => installed,
+  }
+
+  service { 'nginx':
+    ensure   => running,
+    provider => init,
+    enable   => true,
+  }
+
+  exec { 'generate_dhparams':
+    command => 'openssl dhparam -out /etc/nginx/dhparams.pem 2048',
+    creates => '/etc/nginx/dhparams.pem',
+  }
+
+  file { '/var/www/':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0750',
+  }
+
+  file { '/etc/nginx/config-bits':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+}
+
+##########################
+# Firmware Download Site #
+##########################
+class firmware_dl() {
+  # TODO: Refactor!!!
+
+  file { '/etc/nginx/config-bits/dl.ffks':
+    ensure  => present,
+    content => template('nginx/config-bits/dl.ffks'),
+    mode    => 755,
+  }
+
+  file { '/etc/nginx/sites-available/dl.ffks':
+    ensure  => present,
+    content => template('nginx/sites/dl.ffks'),
+    mode    => 755,
+  }
+
+  file { '/etc/nginx/sites-enabled/dl.ffks':
+    ensure => 'link',
+    target => '/etc/nginx/sites-available/dl.ffks',
+  }
+
+  file { '/etc/nginx/sites-available/dl.freifunk-kassel.de':
+    ensure  => present,
+    content => template('nginx/sites/dl.freifunk-kassel.de'),
+    mode    => 755,
+  }
+
+  file { '/etc/nginx/sites-enabled/dl.freifunk-kassel.de':
+    ensure => 'link',
+    target => '/etc/nginx/sites-available/dl.freifunk-kassel.de',
+  }
+}
+
 ###############################
 # Grafana: Network Monitoring #
 ###############################
@@ -12,7 +82,8 @@ class grafana(
   }
 
   # install packages
-  package { ['grafana']:
+  package { 'grafana':
+    require => Exec['apt-update'],
     ensure => installed,
   }
 
